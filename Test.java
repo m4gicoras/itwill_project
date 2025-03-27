98548ada637ee7276d4cde4974625768a6283d9a
fort4 <95965322+fort4@users.noreply.github.com>
2025-03-27 17:31:32 +0900

    Update and rename test.java to Test.java

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) throws InterruptedException {
+        String word = "ITWILL";
+        for (char ch : word.toCharArray()) {
+            System.out.print(ch);
+            Thread.sleep(500);
+        }
+        System.out.println();
+    }
+}
