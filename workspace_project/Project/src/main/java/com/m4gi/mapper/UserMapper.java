68527383e5d80845037d553e8ce7c7042cd32450
jegmin3 <lunate0322@gmail.com>
2025-04-01 20:08:59 +0900

    UserMapper에 insertUser 메서드 추가 (회원가입용)

--- workspace_project/Project/src/main/java/com/m4gi/mapper/UserMapper.java
+++ workspace_project/Project/src/main/java/com/m4gi/mapper/UserMapper.java
@@ -5,4 +5,7 @@ import com.m4gi.domain.User;
 public interface UserMapper {
     // username을 이용해 사용자 정보를 조회합니다.
     User selectUserByUsername(String username);
+
+    // 사용자 정보를 DB에 저장합니다.
+    void insertUser(User user);
 }
