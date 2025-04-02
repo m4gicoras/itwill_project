6001d6fffa3d7838de631e59a335b871a29d1902
jegmin3 <lunate0322@gmail.com>
2025-04-02 17:01:02 +0900

    User, SiteUser에 Lombok @Data 어노테이션 적용

--- workspace_project/Project/src/main/java/com/m4gi/domain/User.java
+++ workspace_project/Project/src/main/java/com/m4gi/domain/User.java
@@ -1,7 +1,9 @@
 package com.m4gi.domain;
 
+import lombok.Data;
 import java.util.Date;
 
+@Data
 public class User {
     private Integer userId;           // 유저 고유 ID (자동 생성)
     private String username;          // 아이디
@@ -17,104 +19,4 @@ public class User {
     private String company_phone;     // 대표 전화 (추가됨)
     private String product_category;  // 상품 카테고리 (추가됨)
     private Date reg_date;            // 가입일 (자동 생성)
-
-    public User() {}
-
-    public Integer getUserId() {
-        return userId;
-    }
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUser_pw() {
-        return user_pw;
-    }
-    public void setUser_pw(String user_pw) {
-        this.user_pw = user_pw;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public String getUser_img() {
-        return user_img;
-    }
-    public void setUser_img(String user_img) {
-        this.user_img = user_img;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getBreg_num() {
-        return breg_num;
-    }
-    public void setBreg_num(String breg_num) {
-        this.breg_num = breg_num;
-    }
-
-    public String getCompany_name() {
-        return company_name;
-    }
-    public void setCompany_name(String company_name) {
-        this.company_name = company_name;
-    }
-
-    public String getMaster_name() {
-        return master_name;
-    }
-    public void setMaster_name(String master_name) {
-        this.master_name = master_name;
-    }
-
-    public String getCompany_addr() {
-        return company_addr;
-    }
-    public void setCompany_addr(String company_addr) {
-        this.company_addr = company_addr;
-    }
-
-    public String getCompany_phone() {
-        return company_phone;
-    }
-    public void setCompany_phone(String company_phone) {
-        this.company_phone = company_phone;
-    }
-
-    public String getProduct_category() {
-        return product_category;
-    }
-    public void setProduct_category(String product_category) {
-        this.product_category = product_category;
-    }
-
-    public Date getReg_date() {
-        return reg_date;
-    }
-    public void setReg_date(Date reg_date) {
-        this.reg_date = reg_date;
-    }
 }
