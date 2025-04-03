0c0caa981995b050ed03f71030579e5d1e636c8f
m4gicoras <m4gicoras@gmail.com>
2025-04-03 09:42:28 +0900

    코드 컨벤션에 맞게 수정정

--- workspace_project/Project/src/main/java/com/m4gi/controller/LoginController.java
+++ workspace_project/Project/src/main/java/com/m4gi/controller/LoginController.java
@@ -17,25 +17,25 @@ public class LoginController {
     // GET 방식으로 로그인 페이지 요청 시
     @GetMapping("/login")
     public String showLoginPage(Model model) {
-        return "main";  
+        return "main";
     }
 
     // POST 방식으로 로그인 요청 처리
     @PostMapping("/login")
-    public String login(@RequestParam("username") String username, //main.jsp에서  username 받아옴
-                        @RequestParam("password") String password, //main.jsp에서  password 받아옴
-                        HttpSession session,
-                        Model model, 
-                        RedirectAttributes redirectAttributes) {
+    public String login(@RequestParam("username") String username, // main.jsp에서 username 받아옴
+            @RequestParam("password") String password, // main.jsp에서 password 받아옴
+            HttpSession session,
+            Model model,
+            RedirectAttributes redirectAttributes) {
 
         boolean authenticated = userService.login(username, password);
         if (authenticated) {
             session.setAttribute("username", username);
             model.addAttribute("success", "로그인 성공!");
-            return "main";  // 로그인 성공 시 대시보드로 포워드, 대시보드 구현 시 수정 필요
+            return "main"; // 로그인 성공 시 대시보드로 포워드, 대시보드 구현 시 수정 필요
         } else {
-        	redirectAttributes.addFlashAttribute("error", "아이디 또는 비밀번호가 일치하지 않습니다.");
-        	return "redirect:/main";
+            redirectAttributes.addFlashAttribute("error", "아이디 또는 비밀번호가 일치하지 않습니다.");
+            return "redirect:/main";
         }
     }
 }
