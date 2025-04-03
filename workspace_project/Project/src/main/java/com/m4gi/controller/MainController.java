f5ef2063c3c970add8b7096f6d725d9e2c465116
fort4 <95965322+fort4@users.noreply.github.com>
2025-04-03 12:38:49 +0900

    Update MainController.java

--- workspace_project/Project/src/main/java/com/m4gi/controller/MainController.java
+++ workspace_project/Project/src/main/java/com/m4gi/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	// http://localhost:8088/web/mainPage
+	// http://localhost:8088/web/main
 	@GetMapping("/main")
 	public String main() {
 		return "main";
