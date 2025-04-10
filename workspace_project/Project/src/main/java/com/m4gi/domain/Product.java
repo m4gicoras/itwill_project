19c57af76ca514df49b03c4d4c27498f3a546220
fort4 <adcretary@gmail.com>
2025-04-10 22:08:04 +0900

    ㅇ

--- workspace_project/Project/src/main/java/com/m4gi/domain/Product.java
+++ workspace_project/Project/src/main/java/com/m4gi/domain/Product.java
@@ -5,16 +5,16 @@ import java.util.Date;
 
 @Data
 public class Product {
-    private int product_id;
-    private int company_id;
-    private String product_name;
-    private String product_desc;
-    private String product_img;
-    private int product_qtty;
-    private String category;
-    private int price;
-    private int status;
-    private Date created_at;
-    private Date updated_at;
+    private int product_id;       // 물품 고유번호
+    private int company_id;		  // 회사(기업) id
+    private String product_name;  // 물품명
+    private String product_desc;  // 물품 설명
+    private String product_img;   // 물품에 연결된 이미지url
+    private int product_qtty;	  // 물품 수량
+    private String category;	  // 물품의 카테고리
+    private int price;			  // 물품의 단가
+    private int status;			  // 물품 상태
+    private Date created_at;	  // 물품 등록일시
+    private Date updated_at;	  // 물품 마지막 업데이트 일시
 }
 
